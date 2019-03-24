cd5614a10fd6cbe0a1e552736d96782b5d6b4976
Karlson <peterpan@tut.by>
2019-03-24 17:52:07 +0300

    feat: create solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let result = number;
+    for(let i = 2; i <= base; i++){
+        if(base % i === 0){
+            let countPrimes = 0;
+            let countDivides = 0;
+            while (base % i === 0){
+                countPrimes++;
+                base = base / i;
+            }
+            let tempNumber = number;
+            while (tempNumber / i > 0){
+                countDivides = countDivides + Math.floor(tempNumber / i);
+                tempNumber = Math.floor(tempNumber / i);
+            }
+            result = Math.min(result, countDivides / countPrimes);
+        }
+    }
+    return  Math.floor(result);
 }
